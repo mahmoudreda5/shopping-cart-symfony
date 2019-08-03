a6bbf05d3fcdda42d387bfcad5768f93596b24aa
mahmoud reda <r.engmahmoud7@gmail.com>
2019-08-03 03:22:11 +0200

    user authentication done, let's begin building app main components

---
+++ src/Controller/AppController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/", name="app_")
+*/
+class AppController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function index()
+    {
+        return $this->render('app/index.html.twig', [
+            'controller_name' => 'AppController',
+        ]);
+    }
+}
